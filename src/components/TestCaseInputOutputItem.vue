50f0a3665e33be4463f800e4414bd84b2ca515a9
Reiner Marquez <rmaceissoft@gmail.com>
2022-11-02 02:16:02 -0500

    only hide or set to readonly input/output fields when decision is set to all

--- src/components/TestCaseInputOutputItem.vue
+++ src/components/TestCaseInputOutputItem.vue
@@ -45,19 +45,31 @@ export default {
         this.objectWithValues[this.inputOutput.name] = newValue;
       },
     },
+    // determine if the input / output should be rendered
+    isHidden() {
+      // an input / ouput shouldnt be rendered when the following conditions are satisfied
+      // 1. the decision of the test case is set to all
+      // 2. it's of type input
+      // 3. the input is binded
+      return this.testCase.decisionId === 'all' && this.type === 'input' && this.inputOutput.binded
+    },
+    // determine is the input / output should be readonly
+    isReadOnly() {
+      return this.testCase.decisionId === 'all' && this.inputOutput.binded
+    }
   },
 };
 </script>
 
 <template>
-  <label :for="inputOutput.id" v-if="type !== 'input' || !inputOutput.binded">
+  <label :for="inputOutput.id" v-if="!isHidden">
     <span>{{ inputOutput.label }}</span>
     <input
       :type="htmlInputType"
       :name="inputOutput.name"
       :id="inputOutput.id"
       v-model="inputOutputValue"
-      :readonly="inputOutput.binded"
+      :readonly="isReadOnly"
     />
     <br />
   </label>
